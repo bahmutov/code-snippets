d553d169e38cd909b1e6a676d29784ccc67b3d36
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-10-22 10:06:39 -0400

    updated profile scope method to support sync methods

--- ng-profile-scope-method.js
+++ ng-profile-scope-method.js
@@ -1,32 +1,34 @@
 /*
   Wraps a method on a scope (found from selector + method name)
   with profiling. After method stops, restores the original non-profiled method
-  Method should return a promise.
+  Method can return a promise.
   Typical use: profile button click.
 
   $scope.myMethod = function () ...
-  where $scope could be determined from element '#my-selector'
+  where $scope could be determined from element 'my-selector'
 */
 (function profileScopeMethod() {
-  var selector = '#my-selector';
-  var methodName = 'myMethod';
+  var selector = 'find';
+  var methodName = 'find';
   var name = selector + ':' + methodName;
 
-  var el = angular.element(selector);
+  var el = angular.element(document.getElementById(selector));
   var scope = el.scope() || el.isolateScope();
   console.assert(scope, 'cannot find scope from ' + name);
 
   var fn = scope[methodName];
   console.assert(typeof fn === 'function', 'missing ' + methodName);
   var $timeout = el.injector().get('$timeout');
+  var $q = el.injector().get('$q');
 
   scope[methodName] = function () {
     console.profile(name);
     console.timeline(name);
     console.time(name);
 
-    // assuming method returns a promise!
-    fn().finally(function finishedMethod() {
+    // method can return a value or a promise
+    var returned = fn();
+    $q.when(returned).finally(function finishedMethod() {
       console.timeStamp('finished', methodName);
 
       $timeout(function afterDOMUpdate() {
@@ -40,5 +42,4 @@
     });
   };
   console.log('wrapped', name, 'for measurements');
- // el.trigger('click');
 }());
