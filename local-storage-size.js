5d2da285465d94f6dd4471af1778d7248969ad57
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-11-26 09:55:07 -0500

    added local storage size code snippet

---
+++ local-storage-size.js
@@ -0,0 +1,28 @@
+// based on answer to question
+// http://stackoverflow.com/questions/4391575/how-to-find-the-size-of-localstorage
+(function showLocalStorageSize(){
+  function stringSizeBytes(str) {
+    return str.length * 2;
+  }
+
+  function toMB(bytes) {
+    return bytes / 1024 / 1024;
+  }
+
+  function toSize(key) {
+    return {
+      name: key,
+      size: stringSizeBytes(localStorage[key])
+    }
+  }
+
+  function toSizeMB(info) {
+    info.size = toMB(info.size).toFixed(2) + ' MB';
+    return info;
+  }
+
+  var sizes = Object.keys(localStorage).map(toSize).map(toSizeMB);
+
+  console.table(sizes);
+
+}());
