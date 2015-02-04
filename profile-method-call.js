fbff9ac5021b3cc1b98f9ed6c08b43abfc9c9dc5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-02-03 22:26:05 -0500

    better profiling messages

--- profile-method-call.js
+++ profile-method-call.js
@@ -1,15 +1,22 @@
-(function profileMethodCall() {
-  var object = window.primesApp;
-  console.assert(object, 'cannot find object to profile');
+(function profileMethodCall(root) {
+  'use strict';
 
+  var name = 'primesApp';
   var methodName = 'findFirstPrimes';
+
+  var object = root[name];
+  console.assert(object, 'cannot find object ' + name + ' to profile');
+
   var originalMethod = object[methodName];
   console.assert(typeof originalMethod === 'function', 'cannot find method ' + methodName);
+
   object[methodName] = function () {
     console.profile(methodName);
     originalMethod.call(object);
     console.profileEnd(methodName);
-    // restore original methodName
+
     object[methodName] = originalMethod;
+    console.log('restored the original method call');
   };
-}());
+  console.log('wrapped', name + '.' + methodName + ' in profiling calls');
+}(this));
