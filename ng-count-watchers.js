0fa0e545a18051218cc5e17b79512d0a2d2bcd9a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-02-18 22:34:04 -0500

    updated count watchers

--- ng-count-watchers.js
+++ ng-count-watchers.js
@@ -1,24 +1,35 @@
 // taken from http://ng.malsup.com/#!/counting-watchers
-(function countAngularWatchers() {
+(function countAngularWatchers(angular) {
   var i, data, scope,
-  count = 0,
-  all = document.all,
-  len = all.length,
-  test = {};
+    count = 0,
+    all = document.all,
+    len = all.length,
+    test = {};
+
+  var mostWatchers = 0;
+
+  function countScopeWatchers(scope, element) {
+    test[scope.$id] = true;
+    var n = scope.$$watchers.length;
+    count += n;
+    if (n > mostWatchers) {
+      console.log('most watchers', n);
+      console.log(element);
+      mostWatchers = n;
+    }
+  }
 
   // go through each element. Count watchers if it has scope or isolate scope
-  /* eslint no-for-loops:0 */
-  for (i = 0; i < len; i++) {
-    /* global angular */
-    data = angular.element(all[i]).data();
+  for (i = 0; i < len; i += 1) {
+    var el = angular.element(all[i]);
+    data = el.data();
     scope = data.$scope || data.$isolateScope;
     if (scope && scope.$$watchers) {
       if ( !test[ scope.$id ] ) {
-        test[ scope.$id ] = true;
-        count += scope.$$watchers.length;
+        countScopeWatchers(scope, el);
       }
     }
   }
   console.log('this page has', count, 'angular watchers');
   return count;
-})();
+}(window.angular));
