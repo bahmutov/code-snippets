47e10224196cdc2dfe82b68dfb03216b7622d2f8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-10-16 17:03:03 -0400

    Added simple digest counter

---
+++ ng-count-digest-cycle-simple.js
@@ -0,0 +1,16 @@
+/*
+  uses console to print a message every time
+  the digest cycle runs.
+*/
+(function monitorDigestCycle(angular) {
+  var injector = angular.element(document.body).injector();
+  if (!injector) {
+    throw new Error('Missing Angular injector on the document body');
+  }
+  var $rootScope = injector.get('$rootScope');
+  function dummy() {
+    console.count('digest cycle');
+  }
+  window.stopWatching = $rootScope.$watch(dummy);
+  console.log('run window.stopWatching() to stop watching the digest cycle');
+}(window.angular));
