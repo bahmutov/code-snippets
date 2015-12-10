e4b635a6d5d354e6e2428569b8743b26adc8b656
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-12-10 15:00:59 -0500

    feat(script): added testing external script injection

--- test-script-injection.js
+++ test-script-injection.js
@@ -3,8 +3,14 @@
   and executing new inline scripts (script-injection attacks)
   See https://github.com/bahmutov/disable-inline-javascript-tutorial
 */
-(function testScriptInjection() {
+(function testInlineScriptInjection() {
   var el = document.createElement('script');
   el.innerText = 'alert("hi there")';
   document.body.appendChild(el); // runs the code by default
+}());
+
+(function testExternalScriptInjection() {
+  var el = document.createElement('script');
+  el.src = 'https://rawgit.com/hakimel/reveal.js/tree/master/js';
+  document.body.appendChild(el);
 }());
