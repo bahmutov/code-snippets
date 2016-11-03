8b70bc0ee4dd4f2c39127092ebf449be269002e0
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-11-03 13:42:35 -0400

    chore(lint): linting JS

--- css-layout.js
+++ css-layout.js
@@ -1,4 +1,9 @@
 // tiny CSS layout "debugger"
 // from https://gist.github.com/addyosmani/fd3999ea7fce242756b1
 // puts random color border around each element
-[].forEach.call($$("*"),function(a){a.style.outline="1px solid #"+(~~(Math.random()*(1<<24))).toString(16)})
+/* global $$ */
+/* jshint -W016 */
+[].forEach.call($$('*'),
+  function (a) {
+    a.style.outline = '1px solid #' + (~~(Math.random() * (1 << 24))).toString(16);
+  });
